8682c3d9a9b773b81d139e937a41a1453ef39ef0
jhollanderpax8 <jhollander@pax8.com>
2024-02-28 15:16:15 -0700

    Fixing things again

--- atreyu_backtrader_api/__init__.py
+++ atreyu_backtrader_api/__init__.py
@@ -4,6 +4,9 @@ from .ibdata import IBData
 from .ibstore import IBStore
 
 __all__ = [
-    'IBStore', 'IBBroker', 'IBData', 'setup_custom_logger',
+    "IBStore",
+    "IBBroker",
+    "IBData",
+    "setup_custom_logger",
 ]
-__version__ = '0.1.0'
+__version__ = "0.1.0"
